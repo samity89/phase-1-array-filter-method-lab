a3806eb164a8e8ecc7987a2cc6c1f6c88c43625c
Samuel Amity <sam.amity@gmail.com>
2022-07-12 12:31:15 -0400

    commit tree dd4665ac86bb361e64288da4b6ab8240bf67fbdf

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, driverName) {
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase() === driverName.toLowerCase();
+    })
+};
+
+function fuzzyMatch (drivers, driverName) {
+    const driverNameLength = driverName.length
+    return drivers.filter(function(driver){
+        return driver.slice(0, driverNameLength) === driverName
+    })
+};
+
+function matchName (drivers, driverName) {
+    const result = drivers.filter(driver => driver.name === driverName)
+    return result
+}
